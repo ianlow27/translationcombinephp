04e93175a8ddb454334b07054059581428e53a77
Ian Low <ianlow27@gmail.com>
2025-12-27 16:37:31 +0000

    0.0.3_251227-1737 - enhancements to translationcombine.php - A number of enhancements and bugfixes including: hide anything enclosed in square-brackets and fix margins, and spacings between starts of chapters. Note that in order to indicate a chapter start the chapter start THAT IS IN ENGLISH in the input file needs to be prefixed at the start of the line with a backtick (`). One to three backticks can be inserted indicating the size of the heading which can be from 120% to 180% of normal font size. The heading is then rendered as center-aligned. This formatting is automatically applied to the other language text.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@
 
 ## NOTES
 
+## 0.0.3_251227-1737 - enhancements to translationcombine.php - A number of enhancements and bugfixes including: hide anything enclosed in square-brackets and fix margins, and spacings between starts of chapters. Note that in order to indicate a chapter start the chapter start THAT IS IN ENGLISH in the input file needs to be prefixed at the start of the line with a backtick (`). One to three backticks can be inserted indicating the size of the heading which can be from 120% to 180% of normal font size. The heading is then rendered as center-aligned. This formatting is automatically applied to the other language text.
+-  Amendments as per the release description
+
 ## 0.0.2_251221-1256 - PHP Code for translationcombine.php. This takes 2 input files a.txt and b.txt where the latter is a translation of the former and merges them sentence by sentence into the output HTML file a.txt_out.html for language acquisition through reading input.
 -  Amendments as per the release description
 
--- translationcombine.php
+++ translationcombine.php
@@ -1,28 +1,87 @@
 <?php
 echo PHP_VERSION;
 $usage = "
-  Usage: php $argv[0] [-h]
-Version: 0.0.2_251221-1256
+  Usage: php $argv[0] [-h|<filename>]
+Version: 0.0.3_251227-1737
   About: $argv[0] Combines 2 translated parallel texts into a single HTML page
  Author: Ian Low | Date: 2025-12-21 | Copyright (c) 2025 Ian Low | License: MIT
 Options:
     -h   Display help information including run options
     -n   Create a new instance
+    <filename>  REQUIRED - this is the input file that must contain both the original text in English and the translated text separated by a line of at least 10 % percentage signs. The line beginning with 3 asterisks indicates the start and end of processing.
 ";
+$inputFile = "";
 if(isset($argv[1])){
   if($argv[1]=="-h"){
     echo $usage;
+    exit();
   }else if($argv[1]=="-n"){  
     echo "Please enter the following information or press 'Enter' for default...\n";
     echo "Project name (defaults to 'myprojphp'): "; $projname = trim(readline());
+    exit();
     if($projname=="") $projname = "myprojphp";
+  }else {
+    $inputFile = $argv[1];
+
   }
+}else {
+    echo $usage;
+    exit();
+
 }
+//-----------------------------------------------------------
+
+echo $inputFile."\n";
+
+$bProcess=false;
+$bEng=true;
+$txtEng="";
+$txtCym="";
+foreach(explode("\n", file_get_contents($inputFile)) as $line){
+  $line = trim($line);
+  if(!$bProcess){
+    if(mb_substr($line, 0,3)=="***"){
+      $bProcess=true;
+      echo "ON___".$line."\n";
+      continue;
+    }
+  }else {
+    if(mb_substr($line, 0,3)=="***"){
+      $bProcess=false;
+      echo "OFF___".$line."\n";
+      continue;
+    }
+  }
+  if(mb_substr($line, 0,10)=="%%%%%%%%%%"){
+    if($bEng){
+      $bEng=false;
+    }
+  }
+
+  if($bProcess){
+    if($bEng){
+      $txtEng.=$line. "\n\n";
+    }else {
+      $txtCym.=$line. "\n\n";
+    }
+  }
+}//endforeach
+file_put_contents($inputFile."_EN.txt", $txtEng);
+file_put_contents($inputFile."_CY.txt", $txtCym);
+
+
+//-----------------------------------------------------------
+
 
 
 // ---- Parameters ----
-$aFile = $argv[1] ?? 'a.txt';
-$bFile = $argv[2] ?? 'b.txt';
+$aFile = $inputFile."_CY.txt"; // $argv[1] ?? 'a.txt';
+$bFile = $inputFile."_EN.txt"; //$argv[2] ?? 'b.txt';
+
+echo $aFile. "____". $bFile. "\n";
+
+
+//exit();
 
 if (!file_exists($aFile) || !file_exists($bFile)) {
     fwrite(STDERR, "Error: Input files not found.\n");
@@ -103,7 +162,8 @@ function splitSentencesXX(string $text): array {
         $text
     );
 }
-$ignoreFullStop = ['Mr.', 'Mrs.', 'mr.', 'mrs.', 'etc.', '&c.'];
+$ignoreFullStop = ['Mr.', 'Mrs.', 'mr.', 'mrs.', 'etc.', '&c.', 'St.', 'st.',
+'A.', 'B.', 'C.', 'D.', 'E.', 'F.', 'G.', 'H.', 'I.', 'J.', 'K.', 'L.', 'M.', 'N.', 'O.', 'P.', 'Q.', 'R.', 'S.', 'T.', 'U.', 'V.', 'W.', 'X.', 'Y.', 'Z.', ];
 
 function splitSentences(string $text): array {
     global $ignoreFullStop;
@@ -133,18 +193,33 @@ function splitSentences(string $text): array {
 
 // ---- Line formatting ----
 function formatLine(string $line, string $type): string {
+    $line = preg_replace('/\[/', "<span class='sphide'>", $line);
+    $line = preg_replace('/\]/', "</span>", $line);
     $style = [];
-    $content = htmlspecialchars($line, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+    $content = $line; //htmlspecialchars($line, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+    $hrLine = "";
 
     if (preg_match('/^(`{1,3})(.*)$/', $line, $m)) {
         $n = strlen($m[1]);
         $content = htmlspecialchars(trim($m[2]), ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
-        $sizes = [1 => '120%', 2 => '150%', 3 => '180%'];
+      if ($type === 'a') {
+        $hrLine = "<br><br><br><hr><br><br>";
+        $sizes = [1 => '120%', 2 => '140%', 3 => '180%'];
         $style = [
             "text-align:center",
+            "color:#000",
             "font-weight:bold",
             "font-size:" . $sizes[$n]
         ];
+      } else {
+        $sizes = [1 => '110%', 2 => '120%', 3 => '130%'];
+        $style = [
+            "text-align:center",
+            "color:#555",
+            "font-weight:bold",
+            "font-size:" . $sizes[$n]
+        ];
+      }
     } elseif (preg_match('/^(¬{1,3})(.*)$/u', $line, $m)) {
         $n = mb_strlen($m[1], 'UTF-8');
         $content = htmlspecialchars(trim($m[2]), ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
@@ -153,17 +228,19 @@ function formatLine(string $line, string $type): string {
             "text-align:right",
             "font-size:" . $sizes[$n]
         ];
+    }else {
+
+      if ($type === 'a') {
+          $style[] = "color:#000";
+          $style[] = "font-size:110%";
+      } else {
+          $style[] = "color:#555";
+          $style[] = "font-size:90%";
+      }
     }
 
-    if ($type === 'a') {
-        $style[] = "color:#000";
-        $style[] = "font-size:110%";
-    } else {
-        $style[] = "color:#555";
-        $style[] = "font-size:90%";
-    }
-
-    return "<div style=\"" . implode("; ", $style) . "\">$content</div>";
+    return $hrLine. "<div style=\"" . implode("; ", $style) . "\">$content</div>";
+    $hrLine = "";
 }
 
 // ---- HTML header ----
@@ -171,22 +248,34 @@ $html = <<<HTML
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta charset="UTF-8">
 <title>Sentence-Aligned Parallel Text</title>
 <style>
+.sphide {
+    display:none;
+}
 body {
     font-family: serif;
-    margin: 2em;
+    xxmargin: 2em;
+    margin: 10px;
     line-height: 1.5;
+    cursor:default;
 }
 .block {
-    margin-left: 2em;
+    margin: 10px;
+    xxmargin-left: 2em;
+    cursor:default;
 }
 .sentence {
-    margin-bottom: 0.3em;
+    margin: 10px;
+    xxmargin-bottom: 0.3em;
+    cursor:default;
 }
 .parabreak {
-    margin-bottom: 1.5em;
+    margin: 10px;
+    xxmargin-bottom: 1.5em;
+    cursor:default;
 }
 </style>
 </head>
